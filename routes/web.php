ffea66bf928e9dddad28cfdb5f1aaacc11da2a77
dominog125 <dominog132@gmail.com>
2024-11-18 19:04:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome1');
 });
 Route::get('/login', [\App\Http\Controllers\AuthManager::class, 'login'])->name('login');
 Route::get('/registration', function () {
