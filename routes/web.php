b982680e79f99ec4fb1ec41bc40a02cd0362b488
dominog125 <dominog132@gmail.com>
2024-11-18 19:07:52 +0100

    CRASH TeSt 4
    Ważne testy

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome1');
+    return view('welcome');
 });
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [\App\Http\Controllers\AuthManager::class, 'login'])->name('login');
+
 Route::get('/registration', function () {
     return view('registration');
 });
